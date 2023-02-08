a3f6f77a61592b1b898967226b55e45aa0aac8a4
Vladimir <ibnteo@gmail.com>
2023-02-08 04:24:31 +0300

    FixPrefix

--- pdoconsole.php
+++ pdoconsole.php
@@ -30,7 +30,7 @@ if (($_POST['query'] ?? false) && ($_SERVER['HTTP_HX_REQUEST'] ?? false)):
 ?>
 <div class="d-flex align-items-baseline mb-3">
 	<button class="btn btn-warning rounded-0 px-4 me-3">Run</button>
-	<label class="font-monospace text-secondary me-3 d-none d-sm-block">C+Enter</label>
+	<label class="font-monospace text-secondary me-3 d-none d-sm-block" title="Ctrl+Enter or Cmd+Enter">C-Enter</label>
 	<div><?php indicator(); ?></div>
 	<div class="font-monospace ms-auto"><?php echo number_format($time, 3, '.', ''); ?> sec, <?php echo $count;?> rows</div>
 </div>
@@ -96,19 +96,20 @@ if (($_POST['query'] ?? false) && ($_SERVER['HTTP_HX_REQUEST'] ?? false)):
 		if ($result) echo 'Ok';
 	endif;
 else:
-	$prefix = htmlspecialchars($_GET['prefix'] ?? '');
+	$prefix = preg_replace('/[^a-z0-9\-_]/i', '', $_GET['prefix'] ?? '');
+	if ($prefix) $prefix .= '-';
 ?>
 <!doctype html>
 <html data-bs-theme="auto">
 <head>
-<title>SQL Console</title>
+<title>SQL Console<?php if ($prefix) echo " [$prefix]"; ?></title>
 <meta charset="utf-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1"/>
 <link rel="icon" href="data:image/svg+xml;base64,<?php echo base64_encode('<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-database" viewBox="0 0 16 16"><path d="M4.318 2.687C5.234 2.271 6.536 2 8 2s2.766.27 3.682.687C12.644 3.125 13 3.627 13 4c0 .374-.356.875-1.318 1.313C10.766 5.729 9.464 6 8 6s-2.766-.27-3.682-.687C3.356 4.875 3 4.373 3 4c0-.374.356-.875 1.318-1.313ZM13 5.698V7c0 .374-.356.875-1.318 1.313C10.766 8.729 9.464 9 8 9s-2.766-.27-3.682-.687C3.356 7.875 3 7.373 3 7V5.698c.271.202.58.378.904.525C4.978 6.711 6.427 7 8 7s3.022-.289 4.096-.777A4.92 4.92 0 0 0 13 5.698ZM14 4c0-1.007-.875-1.755-1.904-2.223C11.022 1.289 9.573 1 8 1s-3.022.289-4.096.777C2.875 2.245 2 2.993 2 4v9c0 1.007.875 1.755 1.904 2.223C4.978 15.71 6.427 16 8 16s3.022-.289 4.096-.777C13.125 14.755 14 14.007 14 13V4Zm-1 4.698V10c0 .374-.356.875-1.318 1.313C10.766 11.729 9.464 12 8 12s-2.766-.27-3.682-.687C3.356 10.875 3 10.373 3 10V8.698c.271.202.58.378.904.525C4.978 9.71 6.427 10 8 10s3.022-.289 4.096-.777A4.92 4.92 0 0 0 13 8.698Zm0 3V13c0 .374-.356.875-1.318 1.313C10.766 14.729 9.464 15 8 15s-2.766-.27-3.682-.687C3.356 13.875 3 13.373 3 13v-1.302c.271.202.58.378.904.525C4.978 12.71 6.427 13 8 13s3.022-.289 4.096-.777c.324-.147.633-.323.904-.525Z"/></svg>'); ?>"/>
 <?php bootstrap(); ?>
 <?php htmx(); ?>
 <script>
-if (localStorage.getItem('theme')) document.documentElement.setAttribute('data-bs-theme', localStorage.getItem('theme'));
+if (localStorage.getItem('pdoconsole-theme')) document.documentElement.setAttribute('data-bs-theme', localStorage.getItem('pdoconsole-theme'));
 window.addEventListener('keydown', function(event) {
 	if (event.key == 'Enter' && (event.ctrlKey || event.metaKey)) {
 		const e = new Event('submit');
@@ -219,31 +220,31 @@ function rowRotate(th) {
 <body class="bg-body-secondary p-2">
 <form action="" method="POST" id="form" hx-post="" hx-target="#result">
 <div class="d-flex align-items-center mb-1">
-	<input class="form-control form-control-sm font-monospace rounded-0" name="dsn" id="dsn" value="" autocomplete="off" placeholder="pgsql:host=localhost; port=5432; dbname=db" onchange="localStorage.setItem('<?php echo $prefix; ?>dsn', this.value)"/>
-	<input class="form-control form-control-sm font-monospace rounded-0" style="max-width:20ch" name="username" id="username" autocomplete="off" placeholder="username" value="" onchange="localStorage.setItem('<?php echo $prefix; ?>username', this.value)"/>
-	<input class="form-control form-control-sm font-monospace rounded-0" style="max-width:20ch" id="passwd" name="passwd" autocomplete="off" type="password" placeholder="passwd" value="" onchange="localStorage.setItem('<?php echo $prefix; ?>passwd', this.value)"/>
+	<input class="form-control form-control-sm font-monospace rounded-0" name="dsn" id="dsn" value="" autocomplete="off" placeholder="pgsql:host=localhost; port=5432; dbname=db" onchange="localStorage.setItem('pdoconsole-<?php echo $prefix; ?>dsn', this.value)"/>
+	<input class="form-control form-control-sm font-monospace rounded-0" style="max-width:20ch" name="username" id="username" autocomplete="off" placeholder="username" value="" onchange="localStorage.setItem('pdoconsole-<?php echo $prefix; ?>username', this.value)"/>
+	<input class="form-control form-control-sm font-monospace rounded-0" style="max-width:20ch" id="passwd" name="passwd" autocomplete="off" type="password" placeholder="passwd" value="" onchange="localStorage.setItem('pdoconsole-<?php echo $prefix; ?>passwd', this.value)"/>
 	<button class="bg-transparent border-0 px-2 text-body" type="button" onclick="
 document.documentElement.setAttribute('data-bs-theme', document.documentElement.getAttribute('data-bs-theme') == 'dark' ? 'light' : 'dark');
-localStorage.setItem('theme', document.documentElement.getAttribute('data-bs-theme'));">
+localStorage.setItem('pdoconsole-theme', document.documentElement.getAttribute('data-bs-theme'));">
 		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-circle-half" viewBox="0 0 16 16">
 		<path d="M8 15A7 7 0 1 0 8 1v14zm0 1A8 8 0 1 1 8 0a8 8 0 0 1 0 16z"/>
 		</svg>
 	</button>
 </div>
-<textarea class="form-control font-monospace rounded-0 mb-3" style="height:20vh; tab-size:4;" id="query" name="query" onkeydown="localStorage.setItem('query', this.value); return keydownQuery(this)" onchange="localStorage.setItem('<?php echo $prefix; ?>query', this.value)"></textarea>
+<textarea class="form-control font-monospace rounded-0 mb-3" style="height:20vh; tab-size:4;" id="query" name="query" onkeydown="localStorage.setItem('pdoconsole-<?php echo $prefix; ?>query', this.value); return keydownQuery(this)" onchange="localStorage.setItem('pdoconsole-<?php echo $prefix; ?>query', this.value)"></textarea>
 <div id="result" class1="overflow-auto">
 	<div class="d-flex align-items-baseline">
 		<button class="btn btn-warning rounded-0 px-4 me-3">Run</button>
-		<label class="font-monospace text-secondary me-3">C+Enter</label>
+		<label class="font-monospace text-secondary me-3" title="Ctrl+Enter or Cmd+Enter">C-Enter</label>
 		<div><?php indicator(); ?></div>
 	</div>
 </div>
 </form>
 <script>
-if (localStorage.getItem('<?php echo $prefix; ?>dsn')) document.getElementById('dsn').value = localStorage.getItem('<?php echo $prefix; ?>dsn');
-if (localStorage.getItem('<?php echo $prefix; ?>username')) document.getElementById('username').value = localStorage.getItem('<?php echo $prefix; ?>username');
-if (localStorage.getItem('<?php echo $prefix; ?>passwd')) document.getElementById('passwd').value = localStorage.getItem('<?php echo $prefix; ?>passwd');
-if (localStorage.getItem('<?php echo $prefix; ?>query')) document.getElementById('query').value = localStorage.getItem('<?php echo $prefix; ?>query');
+if (localStorage.getItem('pdoconsole-<?php echo $prefix; ?>dsn')) document.getElementById('dsn').value = localStorage.getItem('pdoconsole-<?php echo $prefix; ?>dsn');
+if (localStorage.getItem('pdoconsole-<?php echo $prefix; ?>username')) document.getElementById('username').value = localStorage.getItem('pdoconsole-<?php echo $prefix; ?>username');
+if (localStorage.getItem('pdoconsole-<?php echo $prefix; ?>passwd')) document.getElementById('passwd').value = localStorage.getItem('pdoconsole-<?php echo $prefix; ?>passwd');
+if (localStorage.getItem('pdoconsole-<?php echo $prefix; ?>query')) document.getElementById('query').value = localStorage.getItem('pdoconsole-<?php echo $prefix; ?>query');
 </script>
 </body>
 </html>
